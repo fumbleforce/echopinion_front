15c1a386d89fdecefad743608401e585d58b7d0c
jorgeer <jfemail3@gmail.com>
2014-09-20 19:59:09 +0200

    initial yeoman template

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('echopinionFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
